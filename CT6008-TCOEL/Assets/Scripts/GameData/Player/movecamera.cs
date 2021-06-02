ed7608532cff5c678849a8ab559941832bfcfb43
Peedeedle <s1806443@connect.glos.ac.uk>
2021-06-02 12:40:35 +0100

    Final updated V3

---
+++ CT6008-TCOEL/Assets/Scripts/GameData/Player/movecamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movecamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey(KeyCode.Z)) {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(-20.41336f, 2.69f, -10), .6f);
+		}
+
+        //0.479506f
+    }
+}
